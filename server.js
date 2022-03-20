f67554310e1ca37eb1edd61958ac16d091c86f12
MuhammadSaadJamil <muhammadsaadjamil1@gmail.com>
2022-03-20 18:10:29 +0500

    Setup done

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const app = Express();
+const dotenv = require('dotenv').config()
+const app = express();
 const port = process.env.POST || 5000;
+const connectToDB = require('./config/mongoConfig')
+const colors = require('colors')
+const errorHandler = require('./middleware/errorMiddleware')
+
+connectToDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server listening at port : ${port} ...`)
